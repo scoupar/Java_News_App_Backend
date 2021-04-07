8ec4d595d46c05538d1faf603b1d381e829ed853
Helen <aguda.helen@gmail.com>
2021-04-07 20:36:59 +0100

    repositories commit

--- backend/src/main/java/com/codeclan/example/SGNNNewsApp/repository/ArticleRepository.java
+++ backend/src/main/java/com/codeclan/example/SGNNNewsApp/repository/ArticleRepository.java
@@ -1,4 +1,9 @@
 package com.codeclan.example.SGNNNewsApp.repository;
 
-public interface ArticleRepository {
+import com.codeclan.example.SGNNNewsApp.model.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
