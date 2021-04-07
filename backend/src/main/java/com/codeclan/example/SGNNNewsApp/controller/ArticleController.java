99ceec2a03768591a264279d28965f58e67dc114
Helen <aguda.helen@gmail.com>
2021-04-07 21:05:51 +0100

    working on routes

--- backend/src/main/java/com/codeclan/example/SGNNNewsApp/controller/ArticleController.java
+++ backend/src/main/java/com/codeclan/example/SGNNNewsApp/controller/ArticleController.java
@@ -1,4 +1,15 @@
 package com.codeclan.example.SGNNNewsApp.controller;
 
+
+import com.codeclan.example.SGNNNewsApp.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ArticleController {
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+
 }
