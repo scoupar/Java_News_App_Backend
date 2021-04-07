886efd57b369051fa9bf7cd5a4852e95e010e151
Helen <aguda.helen@gmail.com>
2021-04-07 16:37:10 +0100

    models annotated

--- src/main/java/com/codeclan/example/SGNNNewsApp/model/Article.java
+++ src/main/java/com/codeclan/example/SGNNNewsApp/model/Article.java
@@ -1,4 +1,89 @@
 package com.codeclan.example.SGNNNewsApp.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "articles")
 public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="article-title")
+    private String articleTitle;
+    @Column(name="body")
+    private String body;
+    @Column(name="date")
+    private LocalDate date;
+    @JsonIgnoreProperties(value="articles")
+    @ManyToOne
+    @JoinColumn(name = "journalist_id", nullable = false)
+    private Journalist journalist;
+    @Column(name="category")
+    private Category category;
+
+
+    public Article(String articleTitle, String body, LocalDate date, Journalist journalist, Category category) {
+        this.articleTitle = articleTitle;
+        this.body = body;
+        this.date = date;
+        this.journalist = journalist;
+        this.category = category;
+
+
+    }
+
+    public Article() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getArticleTitle() {
+        return articleTitle;
+    }
+
+    public void setArticleTitle(String articleTitle) {
+        this.articleTitle = articleTitle;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Journalist getJournalist() {
+        return journalist;
+    }
+
+    public void setJournalist(Journalist journalist) {
+        this.journalist = journalist;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
--- src/main/java/com/codeclan/example/SGNNNewsApp/model/Category.java
+++ src/main/java/com/codeclan/example/SGNNNewsApp/model/Category.java
@@ -1,4 +1,11 @@
 package com.codeclan.example.SGNNNewsApp.model;
 
 public enum Category {
+
+    News,
+    Politics,
+    Sports,
+    Entertainment,
+    Brexit
+
 }
