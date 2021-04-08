d2e51132bf33e8b61b3c1850e40ba4e1e10ed378
student <student@students-MacBook-Pro.local>
2021-04-08 14:20:38 +0100

    adding folder structure

--- frontend/sgnnnews/src/App.js
+++ frontend/sgnnnews/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import React from 'react';
+import MainContainer from './containers/MainContainer';
 
 function App() {
   return (
     <div className="App">
-{/* {testing} */}
-{/* testing 2 */}
+      <MainContainer />
     </div>
   );
 }
